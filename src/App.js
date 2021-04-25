48155c2687a204590a79ad6b8ec61d67840beff2
mielortiz <mielortiz@gmail.com>
2021-04-25 23:25:56 +0800

    What Dino
    Dinosaur quiz game first commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import { waitForDomChange } from '@testing-library/dom';
+import React, { useState } from 'react';
+
+import questions from './que_ans.js'
+
+export default function App() {
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+	const [quecount, setQuecount] = useState(1);
+	const queItems = [];
+
+	const refreshPage = ()=>{
+		window.location.reload();  
+	}
+
+	if ( queItems.length === 0 ) {
+		for (var i=0; i < questions.length; i++) {
+			queItems.push(i);
+		} 
+	}
+
+	const [items, setItems] = useState(queItems);
+
+	//const randomInit = Math.floor(Math.random() * items.length);
+
+	items.sort(()=> 0.5 - Math.random());
+	const randomItem = items[0];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	console.log(currentQuestion);
+
+	const handleAnswerButtonClick = (isCorrect) => {
+		if (isCorrect) {
+			alert ("That is correct. Good job!");
+			setScore(score + 1);
+		} else {
+			alert ("That is wrong. Nice try!")
+		}
+
+		console.log("randominside " + randomItem);
+		for( var y = 0; y < items.length; y++){ 
+    
+			if ( items[y] === currentQuestion) { 
+				items.splice(y, 1); 
+				y--;
+				setItems(items);
+			}
+		}
+
+		setQuecount(quecount + 1);
+		if(quecount < 10) {
+			setCurrentQuestion(items[0]);
+		} else {
+			setShowScore(true);
+		}
+	}
+
+	//Sorting Answer Set
+	const sortAnswers = questions[currentQuestion].answerOptions;
+	sortAnswers.sort(()=> 0.5 - Math.random());
+
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>You scored {score} out of 10
+					<br></br><button onClick={refreshPage}>Another game?</button>
+				</div>
+				
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>What dinousaur is this?</span>
+						</div>
+						<img className='img-dino' src={questions[currentQuestion].image} alt="" />
+					</div>
+					<div className='answer-section'>
+						{sortAnswers.map((answerOptions, index) => (
+							<button key={index} onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}>{answerOptions.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
---
+++ src/que_ans.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        image: 'http://images.dinosaurpictures.org/dinosaurs-tyrannosaurus_00399363_347a.jpg',
+        answerOptions: [
+            { answerText: 'Tyrannosaurus Rex', isCorrect: true },
+            { answerText: 'Aviatyrannis', isCorrect: false },
+            { answerText: 'Suskityrannus', isCorrect: false },
+        ],
+    },
+    {   
+        image:'http://images.dinosaurpictures.org/triceratops5_0fb0.jpg',
+        answerOptions: [
+            { answerText: 'Saltriosaurus', isCorrect: false },
+            { answerText: 'Triceratops', isCorrect: true },
+            { answerText: 'Eotriceratops', isCorrect: false },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/pterodactyl-adrian-chesterman_54f6.jpg',
+        answerOptions: [
+            { answerText: 'Pterodactyl', isCorrect: true },
+            { answerText: 'Triceratops', isCorrect: false },
+            { answerText: 'Apatosaurus', isCorrect: false },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/velociraptor_12_c4fd.jpg',
+        answerOptions: [
+            { answerText: 'Pterodactyl', isCorrect: false },
+            { answerText: 'Triceratops', isCorrect: false },
+            { answerText: 'Velociraptor', isCorrect: true },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/papo_brachiosaurus_2012_cf3f.jpg',
+        answerOptions: [
+            { answerText: 'Velociraptor', isCorrect: false },
+            { answerText: 'Triceratops', isCorrect: false },
+            { answerText: 'Brachiosaurus', isCorrect: true },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/spinosaurus_2710.jpg',
+        answerOptions: [
+            { answerText: 'Velociraptor', isCorrect: false },
+            { answerText: 'Brachiosaurus', isCorrect: false },
+            { answerText: 'Spinosaurus', isCorrect: true },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/allosaurus_papo_a699.jpg',
+        answerOptions: [
+            { answerText: 'Spinosaurus', isCorrect: false },
+            { answerText: 'Stegosaurus', isCorrect: false },
+            { answerText: 'Allosaurus', isCorrect: true },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/127544_Dilophosaurus_Pose_2_000.jpg0c59dae3-f8e3-48a4-a5d4-da8ff572cef3Original_eb0a.jpg',
+        answerOptions: [
+            { answerText: 'Archaeopteryx', isCorrect: false },
+            { answerText: 'Triceratops', isCorrect: false },
+            { answerText: 'Dilophosaurus', isCorrect: true },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/phpThumb_f3ab.jpg',
+        answerOptions: [
+            { answerText: 'Archaeopteryx', isCorrect: false },
+            { answerText: 'Allosaurus', isCorrect: false },
+            { answerText: 'Stegosaurus', isCorrect: true },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/archaeopteryx_2_fbd3.jpg',
+        answerOptions: [
+            { answerText: 'Stegosaurus', isCorrect: false },
+            { answerText: 'Triceratops', isCorrect: false },
+            { answerText: 'Archaeopteryx', isCorrect: true },
+        ],
+    },
+    {
+        image:'http://images.dinosaurpictures.org/apatosaurusVN_7a58.jpg',
+        answerOptions: [
+            { answerText: 'Archaeopteryx', isCorrect: false },
+            { answerText: 'Velociraptor', isCorrect: false },
+            { answerText: 'Apatosaurus', isCorrect: true },
+        ],
+    },
+];
+
+export default questions;
